2bbf92520dd7163331900c9619400427552d7448
jgotti1 <96301204+jgotti1@users.noreply.github.com>
2022-07-06 08:31:00 -0400

    stucture setup

---
+++ client/src/components/createStudent/createStudent.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
+export default function Create() {
+  const classes = useStyles();
+  const [student, setStudent] = useState({
+    regNo: 0,
+    studentName: "",
+    grade: "",
+    section: "",
+  });
+
+  return (
+    <>
+      <h2>Create Student</h2>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Registration #"
+          variant="outlined"
+          value={student.regNo}
+          onChange={(event) => {
+            setStudent({ ...student, regNo: event.target.value });
+          }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          value={student.studentName}
+          onChange={(event) => {
+            setStudent({ ...student, studentName: event.target.value });
+          }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Grade"
+          variant="outlined"
+          value={student.grade}
+          onChange={(event) => {
+            setStudent({ ...student, grade: event.target.value });
+          }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Section"
+          variant="outlined"
+          value={student.section}
+          onChange={(event) => {
+            setStudent({ ...student, section: event.target.value });
+          }}
+        />
+        <Button variant="contained" color="primary">
+          Create
+        </Button>
+      </form>
+    </>
+  );
+}
